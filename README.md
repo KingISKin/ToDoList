9bcca1513d65fab90701d99856a5a16bdd533ac4
¨KingISkin¨ <joaovictornunes076@gmail.com>
2023-12-28 15:31:50 -0300

    Implementação de banco de dados de login

--- README.md
+++ README.md
@@ -3,8 +3,17 @@ Projeto Iniciante para desenvolvimento Pessoal
 
 # Bibliotecas necessárias:
 tkinter
+sqlite3
 
 # Dev
 João V.
 
+# Lista de planos do projeto.
 
+Criação de To Do List básico, apenas utilizando o console, para testar funcionalidades básicas e planejar novas idéias.
+Adicionar Interface Gráfica simples em Python.
+Tela de Login e senha para acessar ao app.
+
+# Lista de Idéias para o projeto
+
+Salvar listas de usuários.
--- ToDoList.py
+++ ToDoList.py
@@ -1,28 +1,93 @@
+#Imports
 import tkinter as tk
 from tkinter import messagebox
+import sqlite3
 
+#Class
 class TodoListApp:
-    def __init__(self, root):
+    def __init__(self, root, db_file="todo_list.db"):
         self.root = root
         self.root.title("To-Do List")
+
+        # Conectar ao banco de dados SQLite
+        self.conn = sqlite3.connect(db_file)
+        self.create_table()
+
+        # Variáveis de login
+        self.username_var = tk.StringVar()
+        self.password_var = tk.StringVar()
+
+        # Página de login
+        self.create_login_page()
+
+    # Criação da tabela de usuários se não existir
+    def create_table(self):
+        query = """
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT,
+            password TEXT
+        )
+        """
+        self.conn.execute(query)
+        self.conn.commit()
+
+    # Interface Gráfica de Login
+    def create_login_page(self):
+        # Rótulos e entradas para login
+        tk.Label(self.root, text="Nome de usuário:").pack(pady=10)
+        username_entry = tk.Entry(self.root, textvariable=self.username_var)
+        username_entry.pack(pady=5)
+
+        tk.Label(self.root, text="Senha:").pack(pady=10)
+        password_entry = tk.Entry(self.root, show="*", textvariable=self.password_var)
+        password_entry.pack(pady=5)
+
+        # Botão de login
+        login_button = tk.Button(self.root, text="Login", command=self.login)
+        login_button.pack(pady=10)
+
+        # Botão para ir para a página de registro
+        register_button = tk.Button(self.root, text="Registrar", command=self.create_register_page)
+        register_button.pack(pady=10)
+
+    # Validação de login no banco
+    def login(self):
+        # Verifica as credenciais no banco de dados
+        username = self.username_var.get()
+        password = self.password_var.get()
+
+        query = "SELECT * FROM users WHERE username=? AND password=?"
+        result = self.conn.execute(query, (username, password)).fetchone()
+
+        if result:
+            # Se as credenciais estiverem corretas, exibe a aplicação principal
+            self.show_todo_list()
+        else:
+            messagebox.showerror("Erro de Login", "Credenciais inválidas. Tente novamente.")
+
+    def show_todo_list(self):
+        # Criação da janela principal da lista de tarefas
+        main_window = tk.Toplevel(self.root)
+        main_window.title("To-Do List")
 
         # Lista de tarefas
         self.tasks = []
 
         # Entrada para adicionar tarefa
-        self.task_entry = tk.Entry(root, width=30)
+        self.task_entry = tk.Entry(main_window, width=30)
         self.task_entry.pack(pady=10)
 
         # Botão para adicionar tarefa
-        add_button = tk.Button(root, text="Adicionar Tarefa", command=self.add_task)
+        add_button = tk.Button(main_window, text="Adicionar Tarefa", command=self.add_task)
         add_button.pack(pady=5)
 
         # Lista para exibir tarefas
-        self.task_listbox = tk.Listbox(root, width=50)
+        self.task_listbox = tk.Listbox(main_window, width=50)
         self.task_listbox.pack()
 
         # Botão para remover tarefa
-        remove_button = tk.Button(root, text="Remover Tarefa", command=self.remove_task)
+        remove_button = tk.Button(main_window, text="Remover Tarefa", command=self.remove_task)
         remove_button.pack(pady=5)
 
     def add_task(self):
@@ -45,6 +110,60 @@ class TodoListApp:
         for task in self.tasks:
             self.task_listbox.insert(tk.END, task)
 
+    # Página de Registro
+    def create_register_page(self):
+        # Oculta a página de login
+        self.root.withdraw()
+
+        # Criação da janela de registro
+        register_window = tk.Toplevel(self.root)
+        register_window.title("Registrar Usuário")
+
+        # Variáveis de registro
+        new_username_var = tk.StringVar()
+        new_password_var = tk.StringVar()
+
+        # Rótulos e entradas para registro
+        tk.Label(register_window, text="Novo usuário:").pack(pady=10)
+        new_username_entry = tk.Entry(register_window, textvariable=new_username_var)
+        new_username_entry.pack(pady=5)
+
+        tk.Label(register_window, text="Nova senha:").pack(pady=10)
+        new_password_entry = tk.Entry(register_window, show="*", textvariable=new_password_var)
+        new_password_entry.pack(pady=5)
+
+        # Botão para registrar novo usuário
+        register_button = tk.Button(register_window, text="Registrar", command=lambda: self.register_user(new_username_var.get(), new_password_var.get(), register_window))
+        register_button.pack(pady=10)
+
+        # Botão para voltar à página de login
+        back_to_login_button = tk.Button(register_window, text="Já possui acesso? Faça seu login!", command=lambda: self.back_to_login(register_window))
+        back_to_login_button.pack(pady=10)
+
+    def register_user(self, new_username, new_password, register_window):
+        # Verifica se o novo usuário já existe
+        query = "SELECT * FROM users WHERE username=?"
+        result = self.conn.execute(query, (new_username,)).fetchone()
+
+        if result:
+            messagebox.showerror("Erro de Registro", "Usuário já existe. Escolha um nome de usuário diferente.")
+        else:
+            # Registra o novo usuário no banco de dados
+            insert_query = "INSERT INTO users (username, password) VALUES (?, ?)"
+            self.conn.execute(insert_query, (new_username, new_password))
+            self.conn.commit()
+            messagebox.showinfo("Registro Concluído", "Usuário registrado com sucesso!")
+
+            # Fecha a janela de registro e mostra a página de login
+            register_window.destroy()
+            self.root.deiconify()
+
+    def back_to_login(self, register_window):
+        # Fecha a janela de registro e mostra a página de login
+        register_window.destroy()
+        self.root.deiconify()
+
+#Final do código
 if __name__ == "__main__":
     root = tk.Tk()
     app = TodoListApp(root)
